7b6e26e1e92a915177eea18b2bdb700f82b2d3fc
Jeff Cleverley <jeffcleverley@gmail.com>
2018-07-19 21:06:36 +0100

    replaced deprecated beans_widget_area() with beans_get_widget_area_output()

--- views/home-widgets.php
+++ views/home-widgets.php
@@ -1,6 +1,5 @@
 <!-- Add the widget ID string- as a class -->
 <div class="uk-container uk-container-large uk-container-center <?php echo $home_widget; ?> widget-wrap">
 <!-- Display the widget area using the widget area ID string-->
-<!-- https://www.getbeans.io/code-reference/functions/beans_widget_area/-->
-	<?php echo beans_widget_area( $home_widget ); ?>
+	<?php echo beans_get_widget_area_output( $home_widget ); ?>
 </div>
